1a98e172989b57e3b5bf38806fca5d5754069f9e
Samantha Houts <samantha.houts@xamarin.com>
2016-04-22 10:43:58 -0700

    [Controls] Added repro for Bugzilla 27729 & 40161

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla27729.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using Xamarin.Forms.CustomAttributes;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Bugzilla, 27729, "Image does not scale with Aspect when inside a StackLayout with a WidthRequest")]
+	public class Bugzilla27729 : TestContentPage
+	{
+		const string image1 = "xamagon.png";
+
+		protected override void Init()
+		{
+			var image = new Image {
+				Aspect = Aspect.AspectFit,
+			};
+
+			var label = new Label { BackgroundColor = Color.Pink };
+
+			var layout = new StackLayout {
+				Padding = 0,
+				HorizontalOptions = LayoutOptions.Fill,
+				VerticalOptions = LayoutOptions.Fill,
+				WidthRequest = 400,
+				BackgroundColor = Color.Green
+			};
+
+			layout.Children.Add(image);
+			layout.Children.Add(label);
+
+			Content = layout;
+
+			image.Source = image1;
+			label.Text = image1;
+		}
+
+#if UITEST
+		[Test]
+		public void Bugzilla27729Test()
+		{
+			RunningApp.WaitForElement(q => q.Marked(image1));
+			RunningApp.Screenshot("I see the Xamagon fit the width of the stacklayout with no Green background on the sides");
+		}
+#endif
+	}
+}
---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla40161.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+using Xamarin.Forms.CustomAttributes;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Bugzilla, 40161, "Layout not invalidated when changing source of image", PlatformAffected.Android)]
+	public class Bugzilla40161 : TestContentPage
+	{
+		const string image1 = "xamagon.png";
+		const string image2 = "crimson.jpg";
+
+		protected override void Init()
+		{
+			var image = new Image {
+				Aspect = Aspect.AspectFit,
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				Margin = 20
+			};
+
+			var label = new Label();
+
+			var layout = new AbsoluteLayout {
+				Padding = 0,
+				HorizontalOptions = LayoutOptions.Fill,
+				VerticalOptions = LayoutOptions.Fill
+			};
+
+			layout.Children.Add(image, new Rectangle(0, 0, 1, 1), AbsoluteLayoutFlags.WidthProportional | AbsoluteLayoutFlags.HeightProportional);
+			layout.Children.Add(label);
+
+			Content = layout;
+
+			image.Source = image1;
+			label.Text = image1;
+
+			Device.StartTimer(TimeSpan.FromSeconds(2), () => {
+				image.Source = image2;
+				label.Text = image2;
+				return false;
+			});
+		}
+
+#if UITEST
+		[Test]
+		public void Bugzilla40161Test()
+		{
+			RunningApp.Screenshot("I see the Xamagon. It fits the screen with a small margin.");
+			RunningApp.WaitForElement(q => q.Marked(image2));
+			RunningApp.Screenshot("I see the face. It fits the screen with a small margin.");
+		}
+#endif
+	}
+}
